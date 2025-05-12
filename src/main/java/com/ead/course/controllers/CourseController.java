0edab1ce237de80152850f52fb4288805321d2c5
Castro <pedriana.castro@capgemini.com>
2025-05-12 19:54:47 -0300

    adicionando o post

--- src/main/java/com/ead/course/controllers/CourseController.java
+++ src/main/java/com/ead/course/controllers/CourseController.java
@@ -1,11 +1,12 @@
 package com.ead.course.controllers;
 
+import com.ead.course.dtos.CourseRecordDto;
 import com.ead.course.models.CourseModel;
 import com.ead.course.services.CourseServiceAPI;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +24,17 @@ public class CourseController {
     public ResponseEntity<List<CourseModel>> getAllCourses() {
         List<CourseModel> courses = courseService.getAllCourses();
         return ResponseEntity.ok(courses);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> saveCourse(
+            @RequestBody
+            @Valid
+            CourseRecordDto courseRecordDto
+    ) {
+        if(courseService.existsByName(courseRecordDto.name())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Course already exists");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseService.save(courseRecordDto));
     }
 }
--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -4,9 +4,12 @@ import com.ead.course.dtos.CourseRecordDto;
 import com.ead.course.models.CourseModel;
 import com.ead.course.repositories.CourseRepository;
 import com.ead.course.services.CourseServiceAPI;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,7 +29,11 @@ public class CourseServiceImpl implements CourseServiceAPI {
 
     @Override
     public CourseModel save(CourseRecordDto courseRecordDto) {
-        return null;
+        var courseModel = new CourseModel();
+        BeanUtils.copyProperties(courseRecordDto, courseModel);
+        courseModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        courseModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return courseRepository.save(courseModel);
     }
 
     @Override
